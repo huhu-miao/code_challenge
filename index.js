ed95283e17f3f80c3546a42c160be06a833b0e51
huhu-miao <102189364+huhu-miao@users.noreply.github.com>
2022-12-30 17:40:04 -0700

    finish coding

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const counterValue = document.querySelector(".counterValue");
-const increaseBtn = document.getElementsByClassName("increase");
-const decreaseBtn = document.getElementsByClassName("decrease");
-const changeBy = document.getElementsById("changeBy");
-console.log(counterValue.textContent);
+const increaseBtn = document.querySelector(".increase");
+const decreaseBtn = document.querySelector(".decrease");
+const changeBy = document.getElementById("changeBy");
+const resetBtn = document.getElementById("reset");
+
 increaseBtn.addEventListener("click", () => {
-  counterValue.textContent = counterValue.textContent + changeBy.valueAsNumber;
+  counterValue.textContent =
+    parseInt(counterValue.textContent) + changeBy.valueAsNumber;
 });
 decreaseBtn.addEventListener("click", () => {
-  counterValue.textContent = counterValue.textContent - changeBy.valueAsNumber;
+  counterValue.textContent =
+    parseInt(counterValue.textContent) - parseInt(changeBy.value);
+});
+
+changeBy.addEventListener("change", () => {
+  const changeValue = changeBy.valueAsNumber;
+
+  if (Number.isNaN(changeValue)) {
+    changeBy.value = 1;
+  } else if (changeValue < 0) {
+    changeBy.value = 1;
+  } else if (changeValue > 10) {
+    changeBy.value = 10;
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  counterValue.textContent = 0;
+  changeBy.value = 1;
 });
